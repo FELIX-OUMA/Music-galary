f64cec8eedef1b9472de7931b2b95919dc938db3
FELIX-OUMA <142996223+FELIX-OUMA@users.noreply.github.com>
2023-12-01 13:01:33 +0300

    deploy

---
+++ main.js
@@ -0,0 +1,31 @@
+let progress =document.getElementById("progress");
+let song =document.getElementById("song");
+let ctrlIcon=document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+
+}
+function playPause(){
+    if(ctrlIcon.classList.contains("fa-pause")){
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+
+
+    }
+    else{
+        song.pay();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+
+    }
+    if (song.play()){
+        setInterval(()=>{
+            progress.value=song.currentTime;
+        },500
+
+        );
+    }
+}
